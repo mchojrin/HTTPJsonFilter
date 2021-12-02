09506d1397252ddefa91d148966a7b5081d4e034
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-12-02 11:24:55 -0300

    Fixed variable name

--- run.php
+++ run.php
@@ -11,13 +11,13 @@ $offerCollection = $httpReader->read($jsonData);
 
 $subcommand = $argv[1];
 
-$ProductIterator = $offerCollection->getIterator();
+$productIterator = $offerCollection->getIterator();
 switch ($subcommand) {
     case 'count_by_price_range':
-        echo iterator_count(new PriceFilterIterator($ProductIterator, $argv[2], $argv[3]));
+        echo iterator_count(new PriceFilterIterator($productIterator, $argv[2], $argv[3]));
         break;
     case 'count_by_vendor_id':
-        echo iterator_count(new VendorIdFilterIterator($ProductIterator, intval($argv[2])));
+        echo iterator_count(new VendorIdFilterIterator($productIterator, intval($argv[2])));
         break;
 }
 
